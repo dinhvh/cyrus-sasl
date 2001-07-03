b0361f3cb1c6a20213960d1de78c566f3ce71638
Rob Siemborski <rjs3@andrew.cmu.edu>
2001-07-03 15:09:29 +0000

    much cleanup to the order of things in here

--- lib/saslint.h
+++ lib/saslint.h
@@ -1,7 +1,7 @@
 /* saslint.h - internal SASL library definitions
  * Rob Siemborski
  * Tim Martin
- * $Id: saslint.h,v 1.33.2.26 2001/07/02 22:50:07 rjs3 Exp $
+ * $Id: saslint.h,v 1.33.2.27 2001/07/03 15:09:29 rjs3 Exp $
  */
 /* 
  * Copyright (c) 2001 Carnegie Mellon University.  All rights reserved.
@@ -49,43 +49,14 @@
 #include <config.h>
 #include "sasl.h"
 #include "saslplug.h"
+#include "saslutil.h"
+#include "prop.h"
 
+/* #define'd constants */
 #define DEFAULT_MAXOUTBUF 8192
+#define CANON_BUF_SIZE 256
 
-
-extern const sasl_utils_t *global_utils;
-
-extern int _sasl_common_init(void);
-extern int _is_sasl_server_active(void);
-
-/* dlopen.c and staticopen.c */
-extern const int _is_sasl_server_static;
-extern int _sasl_get_mech_list(const char *entryname,
-			       const sasl_callback_t *getpath_callback,
-			       const sasl_callback_t *verifyfile_callback,
-			       int (*add_plugin)(void *,void *));
-extern int _sasl_done_with_plugin(void *plugin);
-extern int _sasl_get_plugin(const char *file,
-			    const char *entryname,
-			    const sasl_callback_t *verifyfile_callback,
-			    void **entrypoint,
-			    void **library);
-
-/* common.c */
-extern const sasl_callback_t *
-_sasl_find_getpath_callback(const sasl_callback_t *callbacks);
-
-extern const sasl_callback_t *
-_sasl_find_verifyfile_callback(const sasl_callback_t *callbacks);
-
-extern void (*_sasl_client_cleanup_hook)(void);
-extern void (*_sasl_server_cleanup_hook)(void);
-
-extern int (*_sasl_client_idle_hook)(sasl_conn_t *conn);
-extern int (*_sasl_server_idle_hook)(sasl_conn_t *conn);
-
-extern int _sasl_strdup(const char *in, char **out, int *outlen);
-
+/* Datatype Definitions */
 typedef struct {
   const sasl_callback_t *callbacks;
   const char *appname;
@@ -107,8 +78,6 @@ typedef struct buffer_info
 enum Sasl_conn_type { SASL_CONN_UNKNOWN = 0,
 		      SASL_CONN_SERVER = 1,
                       SASL_CONN_CLIENT = 2 };
-
-#define CANON_BUF_SIZE 256
 
 struct sasl_conn {
   enum Sasl_conn_type type;
@@ -219,6 +188,118 @@ typedef struct sasl_client_conn {
 
 } sasl_client_conn_t;
 
+typedef struct sasl_allocation_utils {
+  sasl_malloc_t *malloc;
+  sasl_calloc_t *calloc;
+  sasl_realloc_t *realloc;
+  sasl_free_t *free;
+} sasl_allocation_utils_t;
+
+typedef struct sasl_mutex_utils {
+  sasl_mutex_alloc_t *alloc;
+  sasl_mutex_lock_t *lock;
+  sasl_mutex_unlock_t *unlock;
+  sasl_mutex_free_t *free;
+} sasl_mutex_utils_t;
+
+typedef struct sasl_log_utils_s {
+  sasl_log_t *log;
+} sasl_log_utils_t;
+
+typedef int sasl_server_getsecret_t(sasl_conn_t *context,
+				    const char *auth_identity,
+				    const char *realm,
+				    sasl_secret_t ** secret);
+typedef int sasl_server_putsecret_t(sasl_conn_t *context,
+				    const char *auth_identity,
+				    const char *realm,
+				    const sasl_secret_t * secret);
+
+typedef int sasl_plaintext_verifier(sasl_conn_t *conn,
+				    const char *userid,
+				    const char *passwd,
+				    const char *service,
+				    const char *user_realm);
+
+struct sasl_verify_password_s {
+    char *name;
+    sasl_plaintext_verifier *verify;
+};
+
+/*
+ * globals & constants
+ */
+/*
+ * common.c
+ */
+extern const sasl_utils_t *global_utils;
+
+extern void (*_sasl_client_cleanup_hook)(void);
+extern void (*_sasl_server_cleanup_hook)(void);
+extern int (*_sasl_client_idle_hook)(sasl_conn_t *conn);
+extern int (*_sasl_server_idle_hook)(sasl_conn_t *conn);
+
+extern sasl_allocation_utils_t _sasl_allocation_utils;
+extern sasl_mutex_utils_t _sasl_mutex_utils;
+
+/*
+ * checkpw.c
+ */
+extern struct sasl_verify_password_s _sasl_verify_password[];
+
+/*
+ * dlopen.c and staticopen.c
+ */
+extern const int _is_sasl_server_static;
+
+/*
+ * server.c
+ */
+/* (this is a function call to ensure this is read-only to the outside) */
+extern int _is_sasl_server_active(void);
+
+/*
+ * Allocation and Mutex utility macros
+ */
+#define sasl_ALLOC(__size__) (_sasl_allocation_utils.malloc((__size__)))
+#define sasl_CALLOC(__nelem__, __size__) \
+	(_sasl_allocation_utils.calloc((__nelem__), (__size__)))
+#define sasl_REALLOC(__ptr__, __size__) \
+	(_sasl_allocation_utils.realloc((__ptr__), (__size__)))
+#define sasl_FREE(__ptr__) (_sasl_allocation_utils.free((__ptr__)))
+
+#define sasl_MUTEX_ALLOC() (_sasl_mutex_utils.alloc())
+#define sasl_MUTEX_LOCK(__mutex__) (_sasl_mutex_utils.lock((__mutex__)))
+#define sasl_MUTEX_UNLOCK(__mutex__) (_sasl_mutex_utils.unlock((__mutex__)))
+#define sasl_MUTEX_FREE(__mutex__) \
+	(_sasl_mutex_utils.free((__mutex__)))
+
+/* function prototypes */
+/*
+ * dlopen.c and staticopen.c
+ */
+extern int _sasl_get_mech_list(const char *entryname,
+			       const sasl_callback_t *getpath_callback,
+			       const sasl_callback_t *verifyfile_callback,
+			       int (*add_plugin)(void *,void *));
+extern int _sasl_done_with_plugin(void *plugin);
+extern int _sasl_get_plugin(const char *file,
+			    const char *entryname,
+			    const sasl_callback_t *verifyfile_callback,
+			    void **entrypoint,
+			    void **library);
+
+/*
+ * common.c
+ */
+extern const sasl_callback_t *
+_sasl_find_getpath_callback(const sasl_callback_t *callbacks);
+
+extern const sasl_callback_t *
+_sasl_find_verifyfile_callback(const sasl_callback_t *callbacks);
+
+extern int _sasl_common_init(void);
+
 extern int _sasl_conn_init(sasl_conn_t *conn,
 			   const char *service,
 			   int secflags,
@@ -228,70 +309,12 @@ extern int _sasl_conn_init(sasl_conn_t *conn,
 			   const char *ipremoteport,
 			   const sasl_callback_t *callbacks,
 			   const sasl_global_callbacks_t *global_callbacks);
-
 extern void _sasl_conn_dispose(sasl_conn_t *conn);
-
-typedef struct sasl_allocation_utils {
-  sasl_malloc_t *malloc;
-  sasl_calloc_t *calloc;
-  sasl_realloc_t *realloc;
-  sasl_free_t *free;
-} sasl_allocation_utils_t;
-
-typedef struct sasl_log_utils_s {
-  sasl_log_t *log;
-} sasl_log_utils_t;
-
-extern sasl_allocation_utils_t _sasl_allocation_utils;
-
-#define sasl_ALLOC(__size__) (_sasl_allocation_utils.malloc((__size__)))
-#define sasl_CALLOC(__nelem__, __size__) \
-	(_sasl_allocation_utils.calloc((__nelem__), (__size__)))
-#define sasl_REALLOC(__ptr__, __size__) \
-	(_sasl_allocation_utils.realloc((__ptr__), (__size__)))
-#define sasl_FREE(__ptr__) (_sasl_allocation_utils.free((__ptr__)))
-
-typedef struct sasl_mutex_utils {
-  sasl_mutex_alloc_t *alloc;
-  sasl_mutex_lock_t *lock;
-  sasl_mutex_unlock_t *unlock;
-  sasl_mutex_free_t *free;
-} sasl_mutex_utils_t;
-
-extern sasl_mutex_utils_t _sasl_mutex_utils;
-
-#define sasl_MUTEX_ALLOC() (_sasl_mutex_utils.alloc())
-#define sasl_MUTEX_LOCK(__mutex__) (_sasl_mutex_utils.lock((__mutex__)))
-#define sasl_MUTEX_UNLOCK(__mutex__) (_sasl_mutex_utils.unlock((__mutex__)))
-#define sasl_MUTEX_FREE(__mutex__) \
-	(_sasl_mutex_utils.free((__mutex__)))
 
 extern sasl_utils_t *
 _sasl_alloc_utils(sasl_conn_t *conn,
 		  sasl_global_callbacks_t *global_callbacks);
-
-/* FIXME: This const here is kind of ugly */
-extern int
-_sasl_free_utils(const sasl_utils_t ** utils);
-
-
-/* Database Stuff */
-typedef int sasl_server_getsecret_t(sasl_conn_t *context,
-				    const char *auth_identity,
-				    const char *realm,
-				    sasl_secret_t ** secret);
-
-typedef int sasl_server_putsecret_t(sasl_conn_t *context,
-				    const char *auth_identity,
-				    const char *realm,
-				    const sasl_secret_t * secret);
-
-extern sasl_server_getsecret_t *_sasl_db_getsecret;
-extern sasl_server_putsecret_t *_sasl_db_putsecret;
-
-extern int
-_sasl_server_check_db(const sasl_callback_t *verifyfile_cb);
-/* End Database Stuff */
+extern int _sasl_free_utils(const sasl_utils_t ** utils);
 
 extern int
 _sasl_getcallback(sasl_conn_t * conn,
@@ -305,7 +328,33 @@ _sasl_log(sasl_conn_t *conn,
 	  const char *fmt,
 	  ...);
 
-/* external plugin (external.c) */
+/* More Generic Utilities in common.c */
+extern int _sasl_strdup(const char *in, char **out, int *outlen);
+
+/* Basically a conditional call to realloc(), if we need more */
+int _buf_alloc(char **rwbuf, unsigned *curlen, unsigned newlen);
+
+/* convert an iovec to a single buffer */
+int _iovec_to_buf(const struct iovec *vec,
+		  unsigned numiov, buffer_info_t **output);
+
+/* Convert between string formats and sockaddr formats */
+int _sasl_iptostring(const struct sockaddr_in *addr,
+		     char *out, unsigned outlen);
+int _sasl_ipfromstring(const char *addr, struct sockaddr_in *out);
+
+/*
+ * Database Stuff (in db_*.c)
+ */
+extern sasl_server_getsecret_t *_sasl_db_getsecret;
+extern sasl_server_putsecret_t *_sasl_db_putsecret;
+
+extern int
+_sasl_server_check_db(const sasl_callback_t *verifyfile_cb);
+
+/*
+ * external plugin (external.c)
+ */
 int external_client_init(const sasl_utils_t *utils,
 			 int max_version,
 			 int *out_version,
@@ -321,27 +370,17 @@ int external_server_init(const sasl_utils_t *utils,
 			 const char *plugname);
 extern sasl_server_plug_t external_server_mech;
 
-
-
-/* config file declarations (config.c) */
+/*
+ * config file declarations (config.c)
+ */
 extern int sasl_config_init(const char *filename);
 extern const char *sasl_config_getstring(const char *key,const char *def);
 extern int sasl_config_getint(const char *key,int def);
 extern int sasl_config_getswitch(const char *key,int def);
 
-/* clear password checking declarations (checkpw.c) */
-typedef int sasl_plaintext_verifier(sasl_conn_t *conn,
-				    const char *userid,
-				    const char *passwd,
-				    const char *service,
-				    const char *user_realm);
-struct sasl_verify_password_s {
-    char *name;
-    sasl_plaintext_verifier *verify;
-};
-
-extern struct sasl_verify_password_s _sasl_verify_password[];
-
+/*
+ * clear password checking declarations (checkpw.c)
+ */
 extern int _sasl_sasldb_set_pass(sasl_conn_t *conn,
 				 const char *user, 
 				 const char *pass,
@@ -350,47 +389,37 @@ extern int _sasl_sasldb_set_pass(sasl_conn_t *conn,
 				 int flags);
 
 #ifdef DO_SASL_CHECKAPOP
-int _sasl_sasldb_verify_apop(sasl_conn_t *conn,
-			     const char *userstr,
-			     const char *challenge,
-			     const char *response,
-			     const char *user_realm);
+extern int _sasl_sasldb_verify_apop(sasl_conn_t *conn,
+				    const char *userstr,
+				    const char *challenge,
+				    const char *response,
+				    const char *user_realm);
 #endif /* DO_SASL_CHECKAPOP */
 
-/* auxprop.c */
-void _sasl_auxprop_free();
-void _sasl_auxprop_lookup(sasl_server_params_t *sparams,
-			  unsigned flags,
-			  const char *user, unsigned ulen);
+/* Auxprop Plugin (checkpw.c) */
+extern int sasldb_auxprop_plug_init(const sasl_utils_t *utils,
+				    int max_version,
+				    int *out_version,
+				    sasl_auxprop_plug_t **plug,
+				    const char *plugname);
 
-/* checkpw.c */
-int sasldb_auxprop_plug_init(const sasl_utils_t *utils,
-			     int max_version,
-			     int *out_version,
-			     sasl_auxprop_plug_t **plug,
-			     const char *plugname);
+/*
+ * auxprop.c
+ */
+extern void _sasl_auxprop_free(void);
+extern void _sasl_auxprop_lookup(sasl_server_params_t *sparams,
+				 unsigned flags,
+				 const char *user, unsigned ulen);
 
-/* canonuser.c */
+/*
+ * canonusr.c
+ */
 void _sasl_canonuser_free();
-int internal_canonuser_init(const sasl_utils_t *utils,
-			    int max_version,
-			    int *out_version,
-			    sasl_canonuser_plug_t **plug,
-			    const char *plugname);
-
-/* The following is defined in common.c */
-/* Basically a conditional call to realloc(), if we need more */
-int _buf_alloc(char **rwbuf, unsigned *curlen, unsigned newlen);
-
-/* convert an iovec to a single buffer */
-int _iovec_to_buf(const struct iovec *vec,
-		  unsigned numiov, buffer_info_t **output);
-
-/* Convert between string formats and sockaddr formats */
-int _sasl_iptostring(const struct sockaddr_in *addr,
-		     char *out, unsigned outlen);
-int _sasl_ipfromstring(const char *addr, struct sockaddr_in *out);
-
+extern int internal_canonuser_init(const sasl_utils_t *utils,
+				   int max_version,
+				   int *out_version,
+				   sasl_canonuser_plug_t **plug,
+				   const char *plugname);
 extern int _sasl_canon_user(sasl_conn_t *conn,
 			    const char *user, unsigned ulen,
 			    const char *authid, unsigned alen,
